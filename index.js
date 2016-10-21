162988988f4eefba15377852d6d63e1007ceb73b
tzengdandan41 <tzengdandan41@gmail.com>
2016-10-21 16:05:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-//app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
    res.send('I am daniel !');
